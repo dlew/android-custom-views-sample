a636f130b194d83d7dcaa9cc2db3ffd12ab36da7
Dan Lew <danlew42@gmail.com>
2014-08-28 19:49:21 -0500

    Added non-custom View sample

--- app/src/main/java/net/danlew/customviews/SampleActivity.java
+++ app/src/main/java/net/danlew/customviews/SampleActivity.java
@@ -2,35 +2,47 @@ package net.danlew.customviews;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SampleActivity extends Activity {
+
+    private ViewGroup mSampleContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_sample);
+
+        mSampleContainer = (ViewGroup) findViewById(R.id.sample_container);
+
+        User user = new User("Dan Lew", R.drawable.user_dan);
+
+        // Binds without a custom View, as demonstration
+        addNonCustomView(user);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.sample, menu);
-        return true;
+    private void addSample(CharSequence title, View sample) {
+        SampleView sampleView = new SampleView(this);
+        sampleView.bind(title, sample);
+        mSampleContainer.addView(sampleView);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    private void addNonCustomView(User user) {
+        SampleView sampleView = new SampleView(this);
+
+        View view = LayoutInflater.from(this).inflate(R.layout.user_not_custom, sampleView, false);
+        TextView nameView = (TextView) view.findViewById(R.id.name);
+        nameView.setText(user.getName());
+        ImageView iconView = (ImageView) view.findViewById(R.id.icon);
+        iconView.setImageResource(user.getIcon());
+
+        sampleView.bind("Without using custom Views", view);
+
+        mSampleContainer.addView(sampleView);
     }
 }
---
+++ app/src/main/java/net/danlew/customviews/SampleView.java
@@ -0,0 +1,37 @@
+package net.danlew.customviews;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * The sample View is a custom View! When will the madness end?!
+ */
+public class SampleView extends LinearLayout {
+
+    private TextView mTitleView;
+
+    public SampleView(Context context) {
+        super(context);
+
+        setOrientation(LinearLayout.VERTICAL);
+        setGravity(Gravity.CENTER);
+
+        LayoutInflater.from(context).inflate(R.layout.sample_view, this, true);
+        mTitleView = (TextView) findViewById(R.id.title);
+    }
+
+    public void bind(CharSequence title, View view) {
+        // Don't allow binding multiple views
+        if (getChildCount() == 2) {
+            removeViewAt(1);
+        }
+
+        mTitleView.setText(title);
+
+        addView(view);
+    }
+}
