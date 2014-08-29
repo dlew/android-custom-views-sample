e5da15cb7a951c110876095cbda397ce56050206
Dan Lew <danlew42@gmail.com>
2014-08-28 19:28:48 -0500

    Added simple User class

---
+++ app/src/main/java/net/danlew/customviews/User.java
@@ -0,0 +1,23 @@
+package net.danlew.customviews;
+
+/**
+ * Represents a user
+ */
+public class User {
+
+    private String mName;
+    private int mIcon;
+
+    public User(String name, int icon) {
+        mName = name;
+        mIcon = icon;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public int getIcon() {
+        return mIcon;
+    }
+}
